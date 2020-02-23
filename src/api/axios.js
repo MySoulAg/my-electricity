7c0b6b0fd89624118e21b22a99f136b719e9c3f2
li.yin <li.yin@ebiz-fooge.com>
2020-02-23 19:13:46 +0800

    添加顶部的进度条。

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,6 +3,7 @@
  */
 
 import axios from 'axios'
+import NProgress from 'nprogress'
 
 axios.defaults.timeout = 10000 //请求超时时间
 axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1'
@@ -10,6 +11,7 @@ axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1'
 /**请求拦截器 */
 axios.interceptors.request.use(
     config => {
+        NProgress.start();
         const token = window.sessionStorage.getItem('token')
         token && (config.headers.Authorization = token)
         return config
@@ -22,6 +24,7 @@ axios.interceptors.request.use(
 /**响应拦截器 */
 axios.interceptors.response.use(
     response => {
+        NProgress.done();
         const res = response.data;
         //根据返回的状态码 作出逻辑处理
         //.......
--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import echarts from "echarts";
+import echarts from "echarts"
 import VueQuillEditor from 'vue-quill-editor'//富文本编辑器
 
 import 'quill/dist/quill.core.css'//富文本样式
 import 'quill/dist/quill.snow.css'
 import 'quill/dist/quill.bubble.css'
+
+import 'nprogress/nprogress.css'//顶部进度条样式
 
 import './assets/icon/iconfont.css' //字体图标
 import './assets/js/elementUI.js' //element-ui的按需导入
